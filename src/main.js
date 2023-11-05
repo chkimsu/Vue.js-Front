58db48d515b2fd61c4c96673834307228f6d6cd0
t-sochoi <soobin.choi@navercorp.com>
2023-11-05 20:32:51 +0900

    2023-11-05

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 // Optional, since every component import their Bootstrap functionality
 // the following line is not necessary
 // import 'bootstrap'
-
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 
 
